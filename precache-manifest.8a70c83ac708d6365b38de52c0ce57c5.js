63b02fda2d956fb7032f14d7603d02ceab0d6d7f
OlgaSavytska <o.savytska77@gmail.com>
2019-09-10 15:36:57 +0300

    Updates

---
+++ precache-manifest.8a70c83ac708d6365b38de52c0ce57c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31b27ca7b1fd5bddb2e1d6f362a7c5d6",
+    "url": "/goit-react-hw-02-reader/index.html"
+  },
+  {
+    "revision": "ce5815f92807c247b941",
+    "url": "/goit-react-hw-02-reader/static/css/main.b3e4821b.chunk.css"
+  },
+  {
+    "revision": "99c69d682fbe2153a850",
+    "url": "/goit-react-hw-02-reader/static/js/2.806c620e.chunk.js"
+  },
+  {
+    "revision": "ce5815f92807c247b941",
+    "url": "/goit-react-hw-02-reader/static/js/main.7f5a1de9.chunk.js"
+  },
+  {
+    "revision": "6ad384aa03afd857de17",
+    "url": "/goit-react-hw-02-reader/static/js/runtime~main.1bfe6297.js"
+  }
+]);
